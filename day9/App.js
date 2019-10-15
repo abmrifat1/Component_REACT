d111a0a434558bb67cd59b1314eea46b5c17b3a6
abmrifat1 <rifatabm1@gmail.com>
2019-10-15 20:27:24 +0600

    day9

--- day9/App.js
+++ day9/App.js
@@ -1,88 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Hello from "./src/components/hello"
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import {color} from "./src/style";
-
-const App= () => {
-  const{basicText}=styles;
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      {/* <SafeAreaView style={{flexDirection:"row",justifyContent:'space-around',alignItems:"stretch",backgroundColor:"#000000",height:200}}>
-        <Text style={[basicText,{backgroundColor:"#ffffff",flex:1}]}>Sample</Text>
-        <Text style={[basicText,{backgroundColor:"red",flex:1}]}>Text</Text>
-
-      </SafeAreaView> */}
-      <View style={{flexDirection:"row",justifyContent:"space-around",alignItems:"stretch",height:200}}>
-        <Text style={{flex:1,backgroundColor:"green",textAlign:"center"}}>ABM</Text>
-        <Text style={{backgroundColor:"red",flex:2,textAlign:"center"}}>ARS</Text>
-        <Text style={{flex:1,backgroundColor:"green",textAlign:"center"}}>RIFAT</Text>
-
-      </View>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  basicText:{fontSize:30,color:color.green},
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+const AppNavigator = createStackNavigator({
+  Hello: {
+    screen: Hello,
   },
 });
 
-export default App;
+export default createAppContainer(AppNavigator);
